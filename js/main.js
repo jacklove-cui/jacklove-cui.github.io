688d8fd67beee447761f28d91412ba3c1587d24f
jacklove-cui <1259389843@qq.com>
2017-03-21 23:02:52 +0800

    new

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
-
+    // $(window).onresize = function(){
+    //     window.location.reload();
+    // }
+    $(window).resize(function(){
+        window.location.reload();
+    });
     go( "#st-control-1",".page");
     go("#st-control-2",".about-me");
     go("#st-control-3",".skill");
@@ -20,11 +25,35 @@ $(document).ready(function () {
         interval: 2000
     })
 
-    var heard = $(".hader ul li");
-    // var list = heard.find();
-    heard.on("click",function (){
-        var i = $(this).index();
-       heard.eq( i ).addClass("haderhover").siblings().removeClass("haderhover");
+
+        var heard = $(".hader ul li");
+        // var list = heard.find();
+        heard.on("click",function (){
+            setTimeout(function(){
+                var i = $(this).index();
+                heard.eq( i ).addClass("haderhover").siblings().removeClass("haderhover");},200);
+
+
+
+
+        })
+
+
+
+    $(window).on("scroll",function () {
+        var sec =$('[id^=st-control]');
+        var scrollTop = $(this).scrollTop();
+        var floorId = "";
+        sec.each(function (index ,Ele) {
+            var offsetTop = $(Ele).offset().top;
+            if(scrollTop >= offsetTop ){
+                floorId = index ;
+            }else{
+                return false;
+            }
+        })
+        var nav = $(".hader ul li")
+        nav.eq( floorId ).addClass("haderhover").siblings().removeClass("haderhover");
     })
 
 
